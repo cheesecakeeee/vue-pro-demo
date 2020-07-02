6dd7729e93ca9e653d9be48dbc8e6e468d804927
zhengcy <zhengcy@cloudream.com>
2020-07-02 17:48:16 +0800

    feat: 修改vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const path = require('path')
+
+const resolve = dir => path.join(__dirname, dir)
+
+const BASE_URL = process.env.NODE_ENV === 'production' ? '/view' : '/'
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: BASE_URL,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('components'))
+  },
+  // 打包时不生成sourceMap，提高打包速度
+  productionSourceMap: false,
+  devServer: {
+    proxy: 'http://localhost:3000'
+  }
 }
