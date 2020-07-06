e07010cd48a85c6e2d3e9a4e5c6ba20d9741219a
zhengcy <zhengcy@cloudream.com>
2020-07-06 14:56:07 +0800

    命名路由

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> -->
+      <!-- 命名路由跳转 -->
+      <router-link :to="{name: 'Home'}">Home</router-link>  |
+      <router-link :to="{name: 'About'}">About</router-link>
     </div>
     <router-view/>
   </div>
