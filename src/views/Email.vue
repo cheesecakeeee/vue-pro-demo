54d0eefb78759f0122bb2a09a3a41720b3445357
zhengcy <zhengcy@cloudream.com>
2020-07-09 16:34:26 +0800

    feat: 跨文件(视图)的兄弟组件传参bus

--- src/views/Email.vue
+++ src/views/Email.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
+  <div class="email">
     email: XXXXXXXXXX@qq.com
+    <div>{{ message }}</div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      message: ''
+    }
+  },
+  mounted () {
+    this.$bus.$on('handleDeliver', msg => {
+      this.message = msg
+    })
+  }
 }
 </script>
 
 <style>
-
+  .email {
+    border: 1px solid rgb(207, 51, 51);
+  }
 </style>
--- src/views/Tel.vue
+++ src/views/Tel.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
+  <div class="tel">
     tel: 135687654356789
+    <button @click="deliverValue">点击我传递值给兄弟组件</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    deliverValue () {
+      this.$bus.$emit('handleDeliver', '兄弟组件传过来的值')
+    }
+  }
 }
 </script>
 
 <style>
-
+.tel {
+  border: 1px solid rgb(46, 115, 179);
+}
 </style>
